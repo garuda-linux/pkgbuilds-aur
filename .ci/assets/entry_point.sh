f512da8db6752238ee53b8a0e5e1829fbc319ad7
Nico Jensch <root@dr460nf1r3.org>
2023-11-16 19:37:19 +0100

    feat: use Docker-based build environment
    The plan is to use docker-in-docker to be able to use Docker containers instead of systemd-nspawn chroots.

---
+++ .ci/assets/entry_point.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+function setup-buildenv() {
+    [[ -z $PACKAGER ]] && PACKAGER='Garuda Builder <team@garudalinux.org>'
+    [[ -z $MAKEFLAGS ]] && MAKEFLAGS="-j$(nproc)"
+    [[ -z $PACKAGE ]] && PACKAGE="all"
+
+    printf "\nPACKAGER=%s\nMAKEFLAGS=%s" "$PACKAGER" "$MAKEFLAGS" >>/etc/makepkg.conf
+
+    # shellcheck disable=1091
+    source PKGBUILD
+    pacman -Syu --noconfirm --needed --asdeps "${makedepends[@]}" "${depends[@]}" || echo "Failed to install dependencies!"
+}
+
+function build-pkg() {
+    sudo -u builder makepkg -s --noconfirm || echo "Failed to build package!"
+}
+
+setup-buildenv
+build-pkg
--- .ci/build-package.sh
+++ .ci/build-package.sh
@@ -1,33 +1,8 @@
 #!/usr/bin/env bash
 
-for dep in mkarchroot makechrootpkg; do
-    command -v "$dep" &>/dev/null || echo "$dep is not installed!"
-done
-
-function setup-buildenv() {
-    # https://wiki.archlinux.org/title/DeveloperWiki:Building_in_a_clean_chroot
-    mkdir -p /build/chroot
-    _CHROOT=/build/chroot
-    _CHROOT_EXEC="arch-nspawn $_CHROOT/root"
-
-    mkarchroot "$_CHROOT/root" base-devel || echo "failed to make chroot!" && exit 1
-
-    PACKAGER='Garuda Builder <team@garudalinux.org>'
-    MAKEFLAGS="-j$(nproc)"
-
-    printf "PACKAGER=%s\nMAKEFLAGS=%s" "$PACKAGER" "$MAKEFLAGS" >"$_CHROOT/root/etc/makepkg.conf"
-
-    # Prepare chroot for our purposes
-    "$_CHROOT_EXEC" <<EOF
-pacman-key --init 
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com 
-pacman-key --lsign-key 3056513887B78AEB
-pacman --noconfirm -U 'https://geo-mirror.chaotic.cx/chaotic-aur/chaotic-'{keyring,mirrorlist}'.pkg.tar.zst'
-echo "[multilib]" >>/etc/pacman.conf && echo "Include = /etc/pacman.d/mirrorlist" >>/etc/pacman.conf
-echo -e "[garuda]\\nInclude = /etc/pacman.d/chaotic-mirrorlist\\n[chaotic-aur]\\nInclude = /etc/pacman.d/chaotic-mirrorlist" >>/etc/pacman.conf 
-echo "" >>/etc/pacman.conf
-pacman -Syu --noconfirm
-EOF
+function exec-cmd() {
+    # Execute a command in the container
+    docker run -it "$_IMAGE" "$@"
 }
 
 function parse-commit() {
@@ -42,20 +17,21 @@ function parse-commit() {
             [[ "$CI_COMMIT_MESSAGE" == *"[deploy $package]"* ]] &&
                 _PKG="$package" &&
                 echo "Requested package build for $package."
-                break
+            break
         done
     else
         echo "No package to build found in commit message. Exiting." && exit 1
     fi
-
-
 }
 
 function build-pkg() {
-    cd "$_PKG" || echo "Could't change into PKGBUILD directory!" && exit 2
-    makechrootpkg -c -r "$_CHROOT" || echo "Failed to build package!" && exit 3
+    _BUILD_DIR="$PWD/$_PKG"
+    docker run -v "$_BUILD_DIR:/home/builder" \
+        -w "/home/builder" \
+        -e PACKAGE="$_PKG" \
+        -e PACKAGER="Garuda Builder" \
+        "$CI_REGISTRY_IMAGE"
 }
 
 parse-commit
-setup-buildenv
 build-pkg
