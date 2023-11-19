d9fe581fde4477573ea0a09b5fa45719b3cb65bd
Nico Jensch <root@dr460nf1r3.org>
2023-11-19 11:57:52 +0100

    ci: update Docker container configuration

--- .ci/assets/entry_point.sh
+++ .ci/assets/entry_point.sh
@@ -1,21 +1,46 @@
 #!/usr/bin/env bash
 
+set -xe
+
+PKGOUT="/home/builder/pkgout/"
+BUILDDIR="/home/builder/build/"
+
+function setup-package-repo() {
+    if [ ! -d /pkgbuilds ]; then mkdir /pkgbuilds; fi
+    chown root:root /pkgbuilds
+    chmod 700 /pkgbuilds
+    pushd /pkgbuilds
+    if [ ! -d .git ]; then
+        git init
+        git remote add origin https://gitlab.com/garuda-linux/pkgsbuilds-aur.git
+    fi
+    git fetch origin main --depth=1
+    git reset --hard origin/main
+    popd
+}
+
 function setup-buildenv() {
-    [[ -z $PACKAGER ]] && PACKAGER="Garuda Builder <team@garudalinux.org>"
-    [[ -z $MAKEFLAGS ]] && MAKEFLAGS="-j$(nproc)"
-    [[ -z $PACKAGE ]] && PACKAGE="all"
+    if [[ -z $PACKAGER ]]; then PACKAGER="Garuda Builder <team@garudalinux.org>"; fi
+    if [[ -z $MAKEFLAGS ]]; then MAKEFLAGS="-j$(nproc)"; fi
+    if [[ -z $PACKAGE ]]; then exit 1; fi
 
     echo "PACKAGER=\"$PACKAGER\"" >>/etc/makepkg.conf
     echo "MAKEFLAGS=$MAKEFLAGS" >>/etc/makepkg.conf
 
-    # shellcheck disable=1091
-    source PKGBUILD
-    pacman -Syu --noconfirm --needed --asdeps "${makedepends[@]}" "${depends[@]}" || echo "Failed to install dependencies!"
+    chown builder:builder "$PKGOUT"
+    chmod 700 "$PKGOUT"
+    pushd "$PKGOUT"
+    find . -mindepth 1 -delete
+    popd
+
+    cp -rT "/pkgbuilds/${PACKAGE}" "${BUILDDIR}"
+    chown -R builder:builder "${BUILDDIR}"
 }
 
 function build-pkg() {
-    sudo -u builder makepkg -s --noconfirm || echo "Failed to build package!"
+    sudo -D "${BUILDDIR}" -u builder PKGDEST="${PKGOUT}" makepkg -s --noconfirm || { echo "Failed to build package!" && exit 1; }
 }
 
+setup-package-repo
 setup-buildenv
 build-pkg
