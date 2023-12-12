0b5bebc7ad9c60577dd3468d3564748dbe535ca4
Nico Jensch <root@dr460nf1r3.org>
2023-12-12 11:00:09 +0100

    ci: build packages via our chaotic-manager in chaotic-v4 container

--- .ci/schedule-package.sh
+++ .ci/schedule-package.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-function exec-cmd() {
-    # Execute a command in the container
-    docker run -it "$_IMAGE" "$@"
-}
-
 function parse-commit() {
     # Parse our commit message for which packages to build
     mapfile -t _PACKAGES < <(find . -mindepth 1 -type d -prune | sed -e '/.\./d' -e 's/.\///g')
@@ -15,8 +10,7 @@ function parse-commit() {
     elif [[ "$CI_COMMIT_MESSAGE" == *"[deploy"*"]"* ]]; then
         for package in "${_PACKAGES[@]}"; do
             [[ "$CI_COMMIT_MESSAGE" == *"[deploy $package]"* ]] &&
-                _PKG="$package" &&
-                echo "$_PKG" > deploy-pkg &&
+                _PKG="$package"
                 echo "Requested package build for $package."
             break
         done
@@ -25,14 +19,10 @@ function parse-commit() {
     fi
 }
 
-function build-pkg() {
-    _BUILD_DIR="$PWD/$_PKG"
-    docker run -v "$_BUILD_DIR:/home/builder" \
-        -w "/home/builder" \
-        -e PACKAGE="$_PKG" \
-        -e PACKAGER="Garuda Builder" \
-        "$CI_REGISTRY_IMAGE"
+function schedule-pkg() {
+    # Schedules a package build on the chaotic-v4 container of immortalis, using chaotic-manager
+    /entry_point.sh schedule --repo "$BUILD_REPO" "$1"
 }
 
 parse-commit
-build-pkg
+schedule-pkg "$_PKG"
