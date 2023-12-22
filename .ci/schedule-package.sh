f8ee4ebf4fc27b9929a3b35f97deaf259dbf39e3
Nico Jensch <root@dr460nf1r3.org>
2023-12-22 11:25:47 +0100

    ci(schedule-package): support automatically deploying packages without commit msg by parsing git diff

--- .ci/schedule-package.sh
+++ .ci/schedule-package.sh
@@ -1,23 +1,53 @@
 #!/usr/bin/env bash
 
-function parse-commit() {
+function prepare-env() {
+    if [[ "$CI_COMMIT_MESSAGE" == *"[deploy"*"]"* ]]; then
+        _FROM_COMMIT_MSG=1
+    else
+        _FROM_COMMIT_MSG=0
+    fi
+
     # Parse our commit message for which packages to build based on folder names
     mapfile -t _PACKAGES < <(find . -mindepth 1 -type d -prune | sed -e '/.\./d' -e 's/.\///g')
+}
 
-    if [[ "$CI_COMMIT_MESSAGE" == *"[deploy all]"* ]]; then
-        _PKG="full_run"
-        echo "Requested a full routine run."
-    elif [[ "$CI_COMMIT_MESSAGE" == *"[deploy"*"]"* ]]; then
+function parse-commit() {
+    if [[ "$_FROM_COMMIT_MSG" == 1 ]]; then
+        if [[ "$CI_COMMIT_MESSAGE" == *"[deploy all]"* ]]; then
+            _PKG="full_run"
+            echo "Requested a full routine run."
+        elif [[ "$CI_COMMIT_MESSAGE" == *"[deploy"*"]"* ]]; then
+            for package in "${_PACKAGES[@]}"; do
+                if [[ "$CI_COMMIT_MESSAGE" == *"[deploy $package]"* ]]; then
+                    _PKG="$package"
+                    echo "Requested package build for $package."
+                    return 0
+                fi
+            done
+            echo "No package to build found in commit message. Exiting." && exit 1
+        else
+            echo "No package to build found in commit message. Exiting." && exit 1
+        fi
+    else
+        return 0
+    fi
+}
+
+parse-gitdiff() {
+    if [[ "$_FROM_COMMIT_MSG" == 0 ]]; then
+        # Compare the last 2 commits
+        _CURRENT_DIFF=$(git --no-pager diff --name-only HEAD~1..HEAD)
+
+        # Check whether relevant folders got changed
         for package in "${_PACKAGES[@]}"; do
-            if [[ "$CI_COMMIT_MESSAGE" == *"[deploy $package]"* ]]; then
-                _PKG="$package"
-                echo "Requested package build for $package."
+            if [[ "$_CURRENT_DIFF" =~ "$package"/ ]]; then
+                _PKG+="$package"
+                echo "Detected changes in $package, scheduling build..."
                 return 0
             fi
         done
-        echo "No package to build found in commit message. Exiting." && exit 1
     else
-        echo "No package to build found in commit message. Exiting." && exit 1
+        return 0
     fi
 }
 
@@ -31,5 +61,7 @@ schedule-package() {
     fi
 }
 
+prepare-env
 parse-commit
+parse-gitdiff
 schedule-package
