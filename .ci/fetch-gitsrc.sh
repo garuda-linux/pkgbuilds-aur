5180a477cd1acfa7df329b26247b9df21a0de68f
Nico Jensch <root@dr460nf1r3.org>
2023-12-29 15:36:18 +0100

    ci(fetch-gitsrc): determine changes via CI_CONFIG rather than makepkg
    This gets us rid of any makepkg invocation and speeds up the entire process by A LOT.

--- .ci/fetch-gitsrc.sh
+++ .ci/fetch-gitsrc.sh
@@ -4,49 +4,50 @@ for dep in curl git; do
     command -v "$dep" &>/dev/null || echo "$dep is not installed!"
 done
 
-# Parse our commit message for which packages to build based on folder names
+# Build a list of valid VCS packages
 mapfile -t _PACKAGES < <(find . -mindepth 1 -type d -prune | sed -e '/.\./d' -e 's/.\///g')
-
-# This is required for makepkg
-# shellcheck source=/dev/null
-source /etc/makepkg.conf
-chown -R nobody:root "$CI_PROJECT_DIR"
-
-# Get a list of all packages containing "-git"
-IFS=$'\n'
 mapfile -t _VCS_PKG < <(printf '%s\n' "${_PACKAGES[@]}" | sed '/-git/!d')
 
 for package in "${_VCS_PKG[@]}"; do
     printf "\nChecking %s...\n" "$package"
-    pushd "$package" || echo "Failed to change into $package!"
 
-    # Get PKGBUILD's variables
-    # shellcheck source=/dev/null
-    source PKGBUILD
+    # Get current commit via .CI_CONFIG and the first occurrence of a git source
+    _CURRENT_COMMIT=$(grep "CI_GIT_COMMIT=" "$package/.CI_CONFIG" | cut -f1)
+    _SOURCE=$(grep -oP '\ssource\s=\s\Kgit.*$\n?' "$package/.SRCINFO")
 
-    # Abort mission if sources() contains a fixed commit
+    # Abort mission if source contains a fixed commit
     for fragment in branch commit tag revision; do
-        if [[ "$sources" == *"$fragment"* ]]; then
-            echo "Can't update pkgver due to fixed $fragment, continuing."
+        if [[ "$_SOURCE" == *"#$fragment="* ]]; then
+            echo "Can't update pkgver due to fixed $fragment, skipping."
             continue
         fi
     done
 
-    # Download and extract sources, skipping deps
-    sudo -Eu nobody makepkg -do
+    # Strip git+ as ls-remote doesn't accept this kind of URL, then 
+    # retrieve latest commit based on current HEAD. This makes the operation
+    # independant from any API and works with any git remote repository
+    _SRC="${_SOURCE//git+/}"
+    _NEWEST_COMMIT=$(git ls-remote "$_SRC" | grep HEAD | cut -f1)
 
-    # Run pkgver function of the sourced PKGBUILD
-    sudo -Eu nobody makepkg --printsrcinfo | tee .SRCINFO &>/dev/null
-
-    if ! git diff --exit-code --quiet; then
-        git add PKGBUILD .SRCINFO
-        git commit -m "chore($package): git-version $pkgver [deploy $package]"
-        git push "$REPO_URL" HEAD:main || git pull --rebase && git push "$REPO_URL" HEAD:main # Env provided via GitLab CI
+    # Finally update CI_GIT_COMMIT
+    if [[ "$_NEWEST_COMMIT" != "$_CURRENT_COMMIT" ]]; then
+        if ! grep "CI_GIT_COMMIT=" "$package/.CI_CONFIG"; then
+            printf "\nCI_GIT_COMMIT=%s" "$_NEWEST_COMMIT" >>"$package/.CI_CONFIG"
+        else
+            sed -i "s/CI_GIT_COMMIT=.*/CI_GIT_COMMIT=$_NEWEST_COMMIT/g" "$package/.CI_CONFIG"
+        fi
     else
         echo "Package already up-to-date."
+        continue
     fi
-
-    # Cleanup stuff left behind, like sources
-    git reset --hard HEAD
-    git clean -fd
 done
+
+# Push back any changes in case of updates, triggering a rebuild for any package which had its
+# .CI_CONFIG updated
+if ! git diff --exit-code --quiet; then
+    git add .
+    git commit -m "chore(git-versions): update current git commits"
+    git push "$REPO_URL" HEAD:main || git pull --rebase && git push "$REPO_URL" HEAD:main # Env provided via GitLab CI
+else
+    echo "No changes to commit."
+fi
